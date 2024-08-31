68aad1f66a2fbc4cdf1817af1b98f9eb197ac476
Umesh8907 <umesh.tandon.51@gmail.com>
2024-08-31 18:13:18 +0530

    2nd

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,8 @@
 import './globals.css';
 import { SessionProvider } from 'next-auth/react';
 import { ReactNode } from 'react';
-import Header from '../components/layout/Header';
-import Footer from '../components/layout/Footer';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
 
 export const metadata = {
   title: 'LMS System',
